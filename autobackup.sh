2578b63ee9c0062e2fd52861e7f5eac0af797f7e
HarrPerson <HarrPerson>
2015-05-20 14:53:40 +0200

    improvements. checks added for successful run

--- autobackup.sh
+++ autobackup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 timestmp=`/bin/date +%Y-%m-%d_%H%M`
-bkproot="/backups"
-snapdir="snaps"
-bkpname="together"
+bkprootdir="."
+snapdir="snap"
+bkpname="bkpexample"
+srcdir="/srcexample"
 
-/usr/bin/rsync -a --delete --progress /media/together/ /backups/$bkpname/
-/sbin/btrfs su sn -r $bkproot/$bkpname $bkproot/$snapdir/$bkpname.$timestmp
+/usr/bin/rsync -a --delete --progress --exclude-from $srcdir/rsyncexcludes.txt --log-file ./logs/log_$timestmp.txt $srcdir $bkprootdir/$bkpname/
+if [ "$?" -eq "0" ]; then
+	/sbin/btrfs subvolume snapshot -r $bkprootdir/$bkpname $bkprootdir/$snapdir/$bkpname.$timestmp
+	if [ "$?" -eq "0" ]; then
+		echo -e "\n\nBackup successfully finished.\n"
+		exit 0
+	else
+		echo -e "\n  -> Error while running btrfs\n\n"
+		exit 2
+	fi
+else
+	echo -e "\n  -> Error while running rsync\n\n"
+	exit 1
+fi
+exit 3
